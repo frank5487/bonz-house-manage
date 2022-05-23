bf8a8f52f158c159e0e0ffed5e715b2391bb96f4
Yung-Jen <frank5d2008@gmail.com>
2022-05-23 19:30:51 -0400

    add mock data properties

--- bonz-manage-api-server/src/main/java/en/upenn/bonz/dubbo/api/controller/MockController.java
+++ bonz-manage-api-server/src/main/java/en/upenn/bonz/dubbo/api/controller/MockController.java
@@ -2,10 +2,7 @@ package en.upenn.bonz.dubbo.api.controller;
 
 import en.upenn.bonz.dubbo.api.config.MockConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/mock")
@@ -15,8 +12,64 @@ public class MockController {
     @Autowired
     private MockConfig mockConfig;
 
-    @GetMapping("/indexMenu")
+    /**
+     * home menu
+     * @return
+     */
+    @GetMapping("/index/menu")
     public String indexMenu() {
         return mockConfig.getIndexMenu();
+    }
+
+    /**
+     * home info
+     * @return
+     */
+    @GetMapping("/index/info")
+    public String indexInfo() {
+        return this.mockConfig.getIndexInfo();
+    }
+    /**
+     * home faq
+     * @return
+     */
+    @GetMapping("/index/faq")
+    public String indexFaq() {
+        return this.mockConfig.getIndexFaq();
+    }
+    /**
+     * home house info
+     * @return
+     */
+    @GetMapping("/index/house")
+    public String indexHouse() {
+        return this.mockConfig.getIndexHouse();
+    }
+
+    /**
+     * search info
+     *
+     * @param type
+     * @return
+     */
+    @GetMapping("/infos/list")
+    public String infosList(@RequestParam("type")Integer type) {
+        switch (type){
+            case 1:
+                return this.mockConfig.getInfosList1();
+            case 2:
+                return this.mockConfig.getInfosList2();
+            case 3:
+                return this.mockConfig.getInfosList3();
+        }
+        return this.mockConfig.getInfosList1();
+    }
+    /**
+     * my info
+     * @return
+     */
+    @GetMapping("/my/info")
+    public String myInfo() {
+        return this.mockConfig.getMy();
     }
 }
